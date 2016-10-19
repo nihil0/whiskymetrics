7a034331a61cbcb03252a0a4edbc8dc0d9943c0e
Neelabh Kashyap <neelabh2@gmail.com>
2016-10-19 08:57:49 +0000

    example script now puts stuff in DB.

--- example.py
+++ example.py
@@ -8,36 +8,52 @@ This scripts illustrates the features of Whiskymetrics so far.
 
 '''
 
-import scrape,scrub,os,re
+import scrape,scrub,dbconfig,time,psycopg2
 
 w = scrape.WhiskyBot()
-db = scrape.WhiskyDB('whisky.db')
+db = scrape.WhiskyDB()
 
-subject = 'Laphroaig Quarter Cask'
+db.crsr.execute("SELECT review_id FROM metadata EXCEPT SELECT review_id FROM review ORDER BY review_id;")
+review_ids = [m[0] for m in db.crsr]
+for i in review_ids:
+    print(i)
+    time.sleep(1)
 
-links = db.get_post_links(name=subject)
-count=0
-if os.path.exists(os.path.expanduser('~/review-corpora')):
-    with open(os.path.join(os.path.expanduser('~/review-corpora'),
-        re.sub('\s','',subject)+'.txt'),'a+') as f:
-        for name,link in links:
-            count+=1
-            print(count)
+    foo = [m for m in db.get_post_links(review_id=i)]
+
+    for m in foo:
+        print(m)
+        try:
+            text = w.get_review_text(url=m[2])
+        except:
+            print('Something went wrong. Skipping...')
+            continue
+
+
+        cleaned_text = scrub.clean(text)
+
+        #Split into paragraphs
+        paragraphs = [' '.join(scrub.tokenize(m)) for m in cleaned_text.split('\n\n')]
+
+
+        (segments,V) = scrub.represent(paragraphs,m[1])
+
+        relevant_idx = scrub.relevant_segment_index(V)
+        print(relevant_idx)
+
+        if relevant_idx:
+            filtered_segments = [segments[m] for m in relevant_idx]
+            review_text = ' '.join(filtered_segments)
             try:
-                text = w.get_review_text(url=link)
-            except:
-                print('Something went wrong. Skipping...')
-                continue
+                db.put_review_text(i,review_text)
+            except psycopg2.IntegrityError:
+                db.dbconn.rollback()
+                print('Already exists. Skipping...')
+                pass
+
+        else:
+            print('No keywords found. Skipping...')
 
 
-            cleaned = scrub.clean(text)
 
-            paragraphs = [' '.join(scrub.tokenize(m)) for m in cleaned.split('\n\n')]
-
-            (segments,V) = scrub.represent(paragraphs,name)
-
-            filtered_segments = [segments[m] for m in scrub.relevant_segment_index(V)]
-
-            f.write(' '.join(filtered_segments))
-
-
+del(db)
