3c1ec8c533ca3a30dffd889d6812174113beb0b9
Neelabh Kashyap <neelabh2@gmail.com>
2016-10-19 09:17:39 +0000

    fixed failing test

--- test_whiskybot.py
+++ test_whiskybot.py
@@ -5,13 +5,13 @@ class TestWhiskyBot(unittest.TestCase):
         self.bot = scrape.WhiskyBot()
 
     def test_legit_post(self):
-        text=self.bot.get_review_text('3rywqp')
+        text=self.bot.get_review_text(post_id='3rywqp')
         with open('static/sample_text.txt','r') as f:
             sample_text = f.read()
 
         self.assertEqual(text,sample_text)
 
-        text = self.bot.get_review_text(url='https://www.reddit.com/r/bourbon/comments/3rywqp/review_59_1792_port_finish/')
+        text = self.bot.get_review_text(url='http://www.reddit.com/r/bourbon/comments/3rywqp/review_59_1792_port_finish/')
         self.assertEqual(text,sample_text)
 
     def test_bad_post(self):
