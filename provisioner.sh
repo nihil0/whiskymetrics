c347c9f46a67e32c6a355065cac032be698ccdd3
Neelabh Kashyap <neelabh2@gmail.com>
2016-11-17 13:54:00 +0000

    Added provisioning script to improve deployment ease.

---
+++ provisioner.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#Install required programs
+sudo apt-get update
+sudo apt-get -y upgrade
+
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev python-pip python3-pip git
+
+# Set up virtualenv
+sudo pip install virtualenv
+
+mkdir ~/.virtualenv
+
+virtualenv -p python3 ~/.virtualenv/whiskymetrics
+
+# Clone repo
+git clone https://github.com/nihil0/WhiskyMetrics.git
+
+# Enter virtualenv
+source ~/.virtualenv/whiskymetrics/bin/activate
